38184ab69f1cd4788a8456d30b0f7d0b53ca011e
zafle <fleutotanne@free.fr>
2024-04-07 20:12:59 +0200

    js function for select dish animation without easing

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,12 +16,57 @@ document.addEventListener("DOMContentLoaded", function() {
 
     dishes.forEach(function(dish) {
         dish.addEventListener('click', function() {
-            let info = dish.querySelector('.restaurant-menu__info');
-            info.classList.toggle('checked');
+            const info = dish.querySelector('.restaurant-menu__info');
+
+            // toggle class for CSS animation
+            // info.classList.toggle('checked');
+
+            // using requestAnimationFrame();
+            let start, previousTimeStamp;
+            let done = false;
+
+            function infoShow(timeStamp) {
+                if (start === undefined) {
+                    start = timeStamp;
+                }
+
+                const elapsed = timeStamp - start;
+
+                if (previousTimeStamp !== timeStamp) {
+                    const count = Math.min(0.2 * elapsed, 60);
+                    if (!info.classList.contains('opened')) {
+                        move = -60 + count;
+                    } else if (info.classList.contains('opened')) {
+                        move = 0 - count;
+                    }
+
+                    info.style.marginRight = move + 'px';
+                    if (count === 60) {
+                        done = true;
+                        info.classList.toggle('opened');
+                    }
+                }
+
+                if (elapsed < 300) {
+                    previousTimeStamp = timeStamp;
+                    if (!done) {
+                        window.requestAnimationFrame(infoShow);
+                    }
+                }
+            }
+
+            window.requestAnimationFrame(infoShow);
         });
+
+
+
     });
 
     //
+
+
+
+
 
 
 
